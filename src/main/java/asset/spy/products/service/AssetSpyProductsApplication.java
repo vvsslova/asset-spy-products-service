41fbb848e1723a1e054870f701f25998fef24e38
Victoria Shilova <vvsslova@mail.ru>
2025-02-20 23:36:01 +0300

    feat(STOCK-72): created Spring Boot frame

---
+++ src/main/java/asset/spy/products/service/AssetSpyProductsApplication.java
@@ -0,0 +1,12 @@
+package asset.spy.products.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetSpyProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetSpyProductsApplication.class, args);
+    }
+}
